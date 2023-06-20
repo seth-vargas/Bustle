0cdfe855e3fe3078d6c2da821c8369cc189ecdc9
Seth Vargas <shvargas01@gmail.com>
2023-06-19 21:11:24 -0500

    Began work on checkout route

--- app.py
+++ app.py
@@ -131,6 +131,27 @@ def logout():
 
 
 # TODO : Checkout
+YOUR_DOMAIN = 'http://localhost:5000'
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': '{{PRICE_ID}}',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
 
 
 @app.after_request
